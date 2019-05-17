d40e1109c163efd0838ade17b8540f462715eafd
Stef Halmans <stefhalmans@ziggo.nl>
2019-05-17 09:13:40 +0200

    Project toegevoegd

---
+++ ScoreBoardApp/DatabaseController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class DatabaseController
+{
+    
+	public DatabaseController()
+	{
+
+	}
+}
---
+++ ScoreBoardApp/ScoreBoardApp/Players.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoardApp
+{
+    public class Information
+    {
+        public List<Players> Players;
+    }
+    public class Players
+    {
+        public string Name;
+        public List<string> Data;
+    }
+}
